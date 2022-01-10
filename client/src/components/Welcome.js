4de098951178e09399169a42a584e57f73c406f3
Natalia <natalia@roberts-mbp.myfiosgateway.com>
2022-01-09 19:03:35 -0500

    A few styling improvements

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,25 +1,39 @@
-
-   
 import React from 'react';
 import '../App.css';
 import './Welcome.css';
-import Menu from './Menu'
-import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-
+const useStyles = makeStyles({
+    bullet: {
+      display: 'inline-block',
+      margin: '0 6px',
+      transform: 'scale(0.1)',
+    },
+  });
 
 function Welcome() {
+
+    const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
+
     return (
         <div className='main-container'>
             <h1>
                 Natalia Harrow
             </h1>
             <p>
-            Software Engineer, <i>currently at<mark className="blue"> <a href="https://newrelic.com/" target="_blank" rel="noreferrer" className="slide-left-right"> New Relic</a></mark></i>
+                <b>Software Engineer,</b> <i>currently at<mark className="blue"> <a href="https://newrelic.com/" target="_blank" rel="noreferrer" className="slide-left-right"> New Relic</a></mark></i>
             </p>
-            <Grid>
+            {/* <div>
                 <Menu></Menu>
-            </Grid>
+            </div> */}
+            <p className='menu'>
+                <a href="/#about" className="slide-left-right-white"> about </a>
+                {bull}
+                <a href="/#resume" className="slide-left-right-white"> resume </a>
+                {bull}
+                <a href="/#contact" className="slide-left-right-white"> contact </a>
+            </p>
         </div>
     );
 }
