10adc2d7092f3b5aa2d3508939429830a09bf35f
Natalia <natalia@roberts-mbp-3.myfiosgateway.com>
2022-01-30 19:02:14 -0500

    New component and routing changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Welcome from './components/Welcome';
 import About from './components/About';
-import ResumeExperience from './components/ResumeExperience';
-import ResumeEducation from './components/ResumeEducation';
-import Skills from './components/Skills';
 import Contact from './components/Contact';
 import { Container, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core';
+import Resume from './components/Resume';
 
 const useStyles = makeStyles((theme) => ({
   aboutGridOut: {
@@ -18,7 +16,6 @@ const useStyles = makeStyles((theme) => ({
   }, 
   aboutGridIn: {
     marginTop: '4%',
-    // marginBottom: '5%'
   }, 
   contactGrid: {
     width: '100vw',
@@ -46,14 +43,8 @@ function App() {
                 <About></About>
               </Grid>
             </Grid>
-            <Grid id="resume" item xs={12} sm={12} md={12} lg={6}>
-              <ResumeExperience></ResumeExperience>
-            </Grid>
-            <Grid item xs={12} sm={12} md={12} lg={6}>
-              <ResumeEducation></ResumeEducation>
-            </Grid>
-            <Grid item xs={12} sm={12} md={12} lg={6}>
-              <Skills></Skills>
+            <Grid id="resume">
+                <Resume></Resume>
             </Grid>
           </Grid>
         </Container>
